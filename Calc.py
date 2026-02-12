4c10ae8a39059cefa4ab391638454bde859959a0
PotlapelliNavya <navyapotlapelli2005@gmail.com>
2026-02-12 14:40:45 +0530

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+
+def click(x):
+    entry.insert(tk.END, x)
+
+def clear():
+    entry.delete(0, tk.END)
+
+def calc():
+    try:
+        result = eval(entry.get())
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, str(result))
+    except:
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, "Error")
+
+root = tk.Tk()
+root.title("Calculator")
+root.configure(bg="black")
+root.resizable(0, 0)
+
+entry = tk.Entry(
+    root, font=("Arial", 20),
+    bg="grey", fg="white",
+    bd=0, justify="right"
+)
+entry.grid(row=0, column=0, columnspan=4, padx=5, pady=5, ipady=5,ipadx=5)
+
+buttons = [
+    "7", "8", "9", "/",
+    "4", "5", "6", "*",
+    "1", "2", "3", "-",
+    "0", ".", "=", "+"
+]
+
+r = 1
+c = 0
+
+for b in buttons:
+    if b == "=":
+        cmd = calc
+    else:
+        cmd = lambda x=b: click(x)
+
+    tk.Button(root,text=b,command=cmd,font=("Arial", 20),bg="orange",fg="white",bd=0,width=5,height=2).grid(row=r, column=c, padx=5, pady=5)
+
+    c += 1
+    if c==4:
+        c = 0
+        r += 1
+tk.Button(root, text="C", command=clear, font=("Arial", 20), bg="red", fg="white", bd=0, width=22, height=2).grid(row=r, column=0, columnspan=4, padx=5, pady=5)
+
+root.mainloop()
